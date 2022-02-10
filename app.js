2106c540d7d3c62daac863212dc6232d9530e13f
VVIvanov1 <ivanovv.tca@gmail.com>
2022-02-11 00:31:20 +0600

    add like and dislike function

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ let corsOptions = {
     // origin: false,
     preflightContinue: true,
     credentials: true,
-    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+    allowedHeaders: "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
     methods:["GET"]
 
 }
