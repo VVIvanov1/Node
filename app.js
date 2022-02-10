4b48d6e8c7f37454c89048c3ad0ef2a81147ec05
VVIvanov1 <ivanovv.tca@gmail.com>
2022-02-10 23:56:53 +0600

    add like and dislike function

--- app.js
+++ app.js
@@ -29,12 +29,12 @@ let corsOptions = {
 
 }
 app.options('*', cors())
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', req.headers.origin);
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+//   });
 
 
 app.enable('trust proxy');
